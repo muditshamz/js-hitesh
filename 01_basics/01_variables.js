352d637604f6848da42c13a6639d9d7be642e0c3
Mudit Sharma <38452959+muditshamz@users.noreply.github.com>
2024-06-17 07:24:17 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123445;
+let accountEmail = 'ms@mudit.com';
+var userName = 'Mudit';
+workCity = 'Jaipur';
+let accountState;
+// accountId = 2 //Not allowed
+console.table([accountId,accountEmail,userName,workCity,accountState]);
+/*
+    Perfer not to use var due to issues in block and functional scope
+*/
