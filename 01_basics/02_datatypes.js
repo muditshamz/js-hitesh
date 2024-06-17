177836dc8d07e796e80642563a433d61e83dcf2d
Mudit Sharma <38452959+muditshamz@users.noreply.github.com>
2024-06-17 07:42:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3); //alert is function of browser api
+
+let name = 'mudit';
+let age = 20;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string = > ""
+//boolean
+// null (standalone value)
+// undefined => value not defined
+// symbol => uniquness
+
+// non primitive
+// Object
+
+console.log(typeof null); // gives object as null is an object
