878c13557819651cfaef0cbea1b91cb47d25e429
Mudit Sharma <38452959+muditshamz@users.noreply.github.com>
2024-06-18 06:25:15 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,3 +11,23 @@ console.log(nanValueInNumber);
 // "33" => 33
 // "33abc" => NaN
 // true =>1 ; false => 0
+
+// ************************ Operations **********************
+
+console.log(1+2+"3"); //33
+console.log("1"+2+4);//124
+console.log(+true); //1
+console.log(+""); //0
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
