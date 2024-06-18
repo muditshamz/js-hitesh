3a5c6764e13e9884d27e385465a8c98d83343fff
Mudit Sharma <38452959+muditshamz@users.noreply.github.com>
2024-06-18 06:08:48 +0000

    DatatypesConversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = '33';
+let nanScore = '33abc';
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+let nanValueInNumber = Number(nanScore);
+// Notice for most datatypes we getg typeof value in all small caps but while in type conversion we use capital eg - Number()
+console.log(typeof valueInNumber);
+console.log(typeof nanValueInNumber);
+console.log(nanValueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; false => 0
