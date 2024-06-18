aa3ed49ec463d3ef9e810828a4d8c02f069f1dcb
Mudit Sharma <38452959+muditshamz@users.noreply.github.com>
2024-06-18 09:15:09 +0000

    memoryAllocation in JS

---
+++ 01_basics/memeoryInJS.js
@@ -0,0 +1,9 @@
+// 2 Types of memory
+//Stack and Heap
+
+// Stack
+/* ALL PRIMITIVE TYPES USES STACK MEMORY, 
+Whenever we have a stack memory we get a copy of it when called */
+
+// Heap
+/* ALL NON PRIMITIVE USE HEAP MEMORY, heap memory data types gives back reference of memory location to access when accessed */
